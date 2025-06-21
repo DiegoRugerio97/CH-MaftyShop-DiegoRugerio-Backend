0755adda6add62ddd7ee2926caef55ac6d05ddbe
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2025-06-21 13:41:34 -0600

    creation of user moongose model and added session dependencies

---
+++ src/DAOs/Models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const userCollection = 'users'
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER"
+    }
+})
+
+export const userModel = mongoose.model(userCollection, userSchema)
